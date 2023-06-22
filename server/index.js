ad9b9a8e208864fe65c6e49095e634a74525bf2e
MosesJ178 <mosesrstar178@gmail.com>
2023-06-23 02:11:51 +0530

    rm security test2

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     credentials: true
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
