beed995ee181b861864695ea0e05b48b53e21f6f
MosesJ178 <mosesrstar178@gmail.com>
2023-06-23 01:03:17 +0530

    cors test2

--- server/index.js
+++ server/index.js
@@ -10,32 +10,37 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://jobapplication-fullstack.vercel.app',
     credentials: true
 }));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://jobapplication-fullstack.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/user',userroute);
-app.use('/job',jobroute);
+app.use('/user', userroute);
+app.use('/job', jobroute);
 
 const connectDB = async () => {
-    try{
+    try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log("db connection is sucessfull");
     }
-    catch(err){
-        console.log("err while connectin to db",err);
+    catch (err) {
+        console.log("err while connectin to db", err);
     }
 }
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT_NO,()=>{
-        console.log(`server is running on port ${process.env.PORT_NO}`);
+    .then(() => {
+        app.listen(process.env.PORT_NO, () => {
+            console.log(`server is running on port ${process.env.PORT_NO}`);
+        })
     })
-})
-.catch((err)=>{
-    console.log(err.message);
-})
+    .catch((err) => {
+        console.log(err.message);
+    })
