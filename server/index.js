905d52c194634992ba6b77b53916791b3333eb5c
MosesJ178 <mosesrstar178@gmail.com>
2023-06-23 00:55:08 +0530

    cors config

--- server/index.js
+++ server/index.js
@@ -14,12 +14,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({extended:false}));
 
