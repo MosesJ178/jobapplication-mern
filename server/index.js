9fc3a159d58f9e135e1292c4191993e372219a28
MosesJ178 <mosesrstar178@gmail.com>
2023-06-23 12:10:24 +0530

    modified path

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://jobapplication-mern.netlify.app/',
+    origin: 'https://jobapplication-mern.netlify.app',
     credentials: true
 }));
 
